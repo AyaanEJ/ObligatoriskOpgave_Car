ce3ed74b15169feaef83955efa52b8da25bd93fa
Ayaan Bin Ejaz Ahmed <sume0126@edu.easj.dk>
2022-03-07 13:06:46 +0100

    Add project files.

---
+++ ObligatoriskOpgave_Car/Car.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Data;
+using System.Xml.Xsl;
+
+namespace ObligatoriskOpgave_Car
+{
+    public class Car
+    {
+        public int id;
+        public string _model;
+        public int _price;
+        public string _licenseplate;
+        
+        public Car(int id, string model, int price, string licenseplate)
+        {
+            this.id = id;
+            Model = model;
+            Price = price;
+            LicensePlate = licenseplate;
+        }
+
+        public Car()
+        {
+        }
+
+        public string Model
+        {
+            get => _model;
+            set
+            {
+                if (value == null) throw new ArgumentNullException("Model", "Model is null");
+                if (value.Length < 4) throw new ArgumentException("Model length must be atleast 3 charaters");
+                _model = value;
+            }
+        }
+
+        public int Price
+        {
+            get => _price;
+            set
+            {
+                if (value <= 0) throw new ArgumentOutOfRangeException("price", "Price must be higher than 0");
+                _price = value;
+                
+                }
+        }
+        public string LicensePlate
+        {
+            get => _licenseplate;
+            set
+            {
+                if (value.Length >= 2 && value.Length <= 7) _licenseplate = value;
+                else throw new ArgumentOutOfRangeException("licenseplate", value, "Wrong licenseplate");
+            }
+        }
+
+        public override string ToString()
+        {
+            return " ID " + id + " Model " + Model + " Price " + Price + " LicensePlate " + LicensePlate;
+        }
+    }
+}
+
+
---
+++ ObligatoriskOpgave_CarTests/CarTests.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ObligatoriskOpgave_Car;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ExceptionServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObligatoriskOpgave_CarTests
+{
+    [TestClass()]
+    public class CarTests
+    {
+        private Car _car;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _car = new Car(1, "Astra", 220000, "YZ42349");
+        }
+
+        [TestMethod()]
+        public void TestConstructor()
+        {
+            Car c1 = new Car(1, "Astra", 220000, "YZ42349");
+
+            Assert.AreEqual(1, c1.id);
+            Assert.AreEqual("Astra", c1.Model);
+            Assert.AreEqual(220000, c1.Price);
+            Assert.AreEqual("YZ42349", c1.LicensePlate);
+        }
+
+        [TestMethod()]
+        public void TestModel()
+        {
+            _car._model = "Astra";
+            Assert.AreEqual("Astra", _car.Model);
+            Assert.ThrowsException<ArgumentNullException>(() => _car.Model = null);
+            Assert.ThrowsException<ArgumentException>(() => _car.Model = "A");
+        }
+
+        [TestMethod()]
+        public void TestPrice()
+        {
+            Assert.AreEqual(220000, _car.Price);
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => _car.Price = 0);
+        }
+        [TestMethod()]
+        public void TestLicensePlate()
+        {
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => _car.LicensePlate = "A");
+            Assert.AreEqual("YZ42349", _car.LicensePlate);
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => _car.LicensePlate = "BV89435JKL");
+        }
+    }
+}
